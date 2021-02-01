e29d7b4aa186938ba83f8416fe110b2755c40855
Micah Powch <powchm@catlin.edu>
2021-01-31 21:32:23 -0800

    initial commit

---
+++ consts.js
@@ -0,0 +1,203 @@
+export const bot_port = 8000;
+export const slash_port = 4050;
+export const json_data_path = 'data.json'
+export const hours_column = 4
+export const name_column = 0;
+export const max_row = 41;
+export const hours_sheet_id = '1-j-sAjyLNufVCZUzr4fnkDNj8JPIN9o8t6_-CuopHSY'
+export const dmRejection = true;
+export const initing = true;
+
+export const log_modal = {
+	"type": "modal",
+	"title": {
+		"type": "plain_text",
+		"text": "Log Work Time",
+		"emoji": true
+	},
+	"submit": {
+		"type": "plain_text",
+		"text": "Submit",
+		"emoji": true
+	},
+	"close": {
+		"type": "plain_text",
+		"text": "Cancel",
+		"emoji": true
+	},
+	"blocks": [
+		// {
+		// 	"type": "image",
+		// 	"image_url": "https://i.ibb.co/WfXVHyv/Untitled.png",
+		// 	"alt_text": "doser gives u love"
+		// },
+		{
+			"type": "input",
+			"block_id": "hours",
+			"element": {
+				"type": "plain_text_input",
+				"action_id": "hours"
+			},
+			"label": {
+				"type": "plain_text",
+				"text": "Hours Spent",
+				"emoji": true
+			}
+		},
+		{
+			"type": "input",
+			"block_id": "task",
+			"element": {
+				"type": "plain_text_input",
+				"multiline": true,
+				"action_id": "task"
+			},
+			"label": {
+				"type": "plain_text",
+				"text": "Activity",
+				"emoji": true
+			}
+		}
+	]
+};
+
+export const getSubmittedBlocks = (hours,activity)=>{
+
+	let blocks = [
+			{
+				"type": "section",
+				"text": {
+					"type": "mrkdwn",
+					"text": `:clock2: You submitted *${hours} hours* for activity\n>_\`\`\`${activity}\`\`\`_`
+				}
+			},
+			{
+				"type": "context",
+				"elements": [
+					{
+						"type": "plain_text",
+						"text": "*TimeBot will let you know when these hours have been approved",
+						"emoji": true
+					}
+				]
+			}
+		]
+
+	return blocks;
+
+}
+
+export const getSubmittedDm = (hours,activity)=>{
+	let s = 's'
+	if(hours===1) {s = ''}
+	return `:clock2: You submitted *${hours} hour${s}* for activity:\n>\`${activity}\``
+	// return `:white_check_mark: You submitted *${hours} hours* for activity:\n\`\`\`${activity}\`\`\``
+	// return `:clock2: You submitted \'${hours}\' hours for activity \'${activity}\'`
+// 
+}
+
+export const getAcceptedDm = (pigChumpId, hours,activity)=>{
+	return `:white_check_mark: *<@${pigChumpId}>* accepted *${hours} hours* for activity:\n>\`${activity}\``
+}
+export const getDeclinedDm = (pigChumpId, hours,activity)=>{
+	return `:x: *<@${pigChumpId}>* declined *${hours} hours* for activity:\n>\`${activity}\``
+}
+
+
+export const getSubmittedDmHrsAndMins = (hrs,mins,activity)=>{
+	let s = 's'
+	if(hrs===1) {s = ''}
+	let sm = 's'
+	if(mins===1) {sm = ''}
+	if(hrs===0) {
+		return `:clock2: You submitted *${mins} minute${sm}* for activity:\n>\`${activity}\``
+	} else if(mins===0) {
+		return `:clock2: You submitted *${hrs} hour${s}* for activity:\n>\`${activity}\``
+	} else {
+		return `:clock2: You submitted *${hrs} hour${s}, ${mins} minute${sm}* for activity:\n>\`${activity}\``
+	}
+	
+	// return `:white_check_mark: You submitted *${hours} hours* for activity:\n\`\`\`${activity}\`\`\``
+	// return `:clock2: You submitted \'${hours}\' hours for activity \'${activity}\'`
+// 
+}
+
+export const getRequestBlockList = (uid,hrs,mins,activity,mid)=>{
+	let s = 's'
+	if(hrs===1) {s = ''}
+	let sm = 's'
+	if(mins===1) {sm = ''}
+	let timeArg;
+	if(hrs===0) {
+		timeArg = `${mins} minute${sm}`
+	} else if(mins===0) {
+		timeArg = `${hrs} hour${s}`
+	} else {
+		timeArg = `${hrs} hour${s}, ${mins} minute${sm}`
+	}
+
+
+
+
+	return [
+		{
+			"type": "header",
+			"text": {
+				"type": "plain_text",
+				"text": "Time Submission",
+				"emoji": true
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": `>>>*<@${uid}>* submitted *${timeArg}* for activity\n\`${activity}\``
+			}
+		},
+		{
+			"type": "actions",
+			"elements": [
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"emoji": true,
+						"text": "Add to Sheet"
+					},
+					"style": "primary",
+					"value": `${mid}`
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"emoji": true,
+						"text": "Decline"
+					},
+					"style": "danger",
+					"value": "decline"
+				}
+			]
+		},
+		{
+			"type": "divider"
+		}
+	]
+}
+
+export const addedFooter = {
+	"type": "section",
+	"text": {
+		"type": "mrkdwn",
+		"text": "*_:white_check_mark: ADDED :white_check_mark:_*"
+	}
+}
+
+export const declinedFooter = {
+	"type": "section",
+	"text": {
+		"type": "mrkdwn",
+		"text": "*_:x: DECLINED :x:_*"
+	}
+}
---
+++ index.js
@@ -0,0 +1,392 @@
+import {WebClient} from "@slack/web-api"
+import {createEventAdapter} from "@slack/events-api"
+import lineByLine from 'n-readlines';
+
+import http from 'http';
+import https from 'https';
+// const {WebClient} = require('@slack/web-api');
+// const{createEventAdapter} = require('@slack/events-api')
+import {bot_port,slash_port,log_modal, getSubmittedDm,getSubmittedDmHrsAndMins, json_data_path, getRequestBlockList, addedFooter, declinedFooter, max_row, name_column, hours_column, hours_sheet_id, getAcceptedDm,getDeclinedDm,dmRejection,initing} from './consts.js'
+import { secret, token } from './slack_secrets.js'
+import { existsSync, readFile, readFileSync, writeFile } from 'fs'
+
+import { promisify } from 'util'
+import { setupMaster } from "cluster";
+import { GoogleSpreadsheet } from "google-spreadsheet";
+
+const writeJSON = async function() {
+    writeFile(json_data_path,JSON.stringify(DATA),(err)=>{console.log(err)})
+}
+
+let DATA
+if(existsSync(json_data_path)) {
+    DATA = JSON.parse(readFileSync(json_data_path))
+} else {
+    DATA = {"e":{}}
+    writeJSON()
+}
+
+const handleHoursRequest = async function(uid,hrs,mins,activity) {
+    if(!('rnum' in DATA)) {
+        DATA['rnum'] = 0;
+    }
+    DATA['rnum'] = DATA['rnum'] + 1;
+
+    // try{
+    const result = await post.users.info({
+        user: uid
+      });
+    // } catch (err) {console.log(err)}
+
+    if(!('e'in DATA)) {DATA['e']={}}
+    DATA['e'][DATA['rnum']] = {name: result['user']['real_name'],time: parseFloat(hrs + mins/60).toFixed(1), userId: uid, activity: activity}
+    writeJSON()
+
+    let requestDmBlocks = getRequestBlockList(uid,hrs,mins,activity,DATA['rnum'])
+    post.chat.postMessage({channel: DATA['myLittlePogchamp'],blocks: requestDmBlocks})
+}
+
+let sheet;
+
+
+///////////////// INIT SLACK 
+
+const events = createEventAdapter(secret);
+const post = new WebClient(token);
+let beeScript;
+let fizz = false;
+
+/////////////////// INIT GOOGLE DRIVE
+
+(async ()=>{
+    const google_client_secret = JSON.parse(readFileSync('./client_secret.json'))
+    const doc = await new GoogleSpreadsheet(hours_sheet_id)
+    await doc.useServiceAccountAuth(google_client_secret)
+    await doc.loadInfo()
+    sheet = doc.sheetsByIndex[0]
+})();
+
+async function addhours(name,hours) {
+    await sheet.loadCells({ startRowIndex: 0, endRowIndex: max_row + 1, startColumnIndex:name_column, endColumnIndex: hours_column+1 })
+    for (let y = 0; y < max_row; y++) {
+        // console.log(y)
+        const name_cell = sheet.getCell(y,name_column)
+
+        if(name.includes(name_cell.value) && name_cell.value!="" && name_cell.value!=" ") {
+            const hours_cell = sheet.getCell(y,hours_column)
+            hours_cell.formula = `${hours_cell.formula}+${parseFloat(hours).toFixed(1)}`
+            hours_cell.save()
+            return
+        }
+        
+    }
+    post.chat.postMessage({channel: DATA['myLittlePogchamp'], text: `:warning: Could not find member *${name}* in spreadsheet`})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////// SLACK EVENTS HANDLER
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+
+const atCommands = {"buzz":async(event)=>{
+    // if (beeScript==undefined) {
+        // beeScript = readline.createInterface({
+        //     input: fs.createReadStream('/path/to/file'),
+        //     output: process.stdout,
+        //     console: false })
+    // }
+    
+    beeScript = new lineByLine('./buzz.txt');
+    let beeLine;
+
+    while((beeLine = beeScript.next().toString()) && !fizz) {
+        // console.log(beeLine);
+        post.chat.postMessage({channel: event.channel, text:beeLine}).catch((err)=>console.log(err));
+        await sleep(1000);
+    }
+    fizz = false;
+
+    // for (let index = 0; index < 100; index++) {
+    //     post.chat.postMessage({channel: event.channel, text:"you like jaAzzZ?"});
+    // }
+    
+},"fizz":async(event)=>{
+    fizz = true;
+},'imyourlittlepogchamp':async(event)=>{
+    if(initing) {
+        DATA['myLittlePogchamp'] = event.user
+        writeJSON()
+        console.log("UPDATED!")
+    }
+    console.log('run!')
+}
+};
+
+events.on('app_mention', (event)=>{
+    //console.log(event.user);
+    //console.log(event.text);
+    let textSplit = event.text.split(" ");
+    if(textSplit[1] in atCommands) {
+        atCommands[textSplit[1]](event);
+    }
+    //console.log("sent!");
+});
+
+events.on('ready',()=>{
+    console.log("ready!");
+});
+
+events.start(bot_port).then(()=>{
+    console.log(`event listener started on port ${bot_port}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+const slashServer = http.createServer(async(request,response)=>{
+
+    if(request.method == "POST") {
+
+        response.writeHead(200);
+        response.end()
+
+        let body = "";
+
+        await request.on('data', function(data) {
+            body += data
+            //console.log('Partial body: ' + '\n' + body)
+        })
+        
+        //console.log(`full body: ${body}`)
+
+        let split = body.split("&");
+        let requestDict = {};
+        split.forEach((value)=>{
+            var i = value.indexOf('=');
+            var keyVal = [value.slice(0,i), value.slice(i+1)];
+ 
+            requestDict[keyVal[0]]=decodeURIComponent(keyVal[1].replace(/\+/g," "));
+            
+        })
+        //console.log(requestDict)
+
+
+
+        if('payload' in requestDict) {
+            // response.writeHead(200,{'Content-type':'application/json'});
+            let real_json = JSON.parse(requestDict['payload']);
+            //console.log(real_json)
+
+            
+            // const data = JSON.stringify({
+            //     "trigger_id": real_json['trigger_id'],
+            //     "dialog": log_modal,
+            //     'token': token,
+            // });
+
+
+            if(real_json['type']=='shortcut') {
+
+            let payload = log_modal;
+            // payload['callback_id'] = real_json['callback_id'];
+            const data = 
+                'token='+ encodeURIComponent(token) +
+                "&view=" + encodeURIComponent(JSON.stringify(payload)) +
+                "&trigger_id="+ encodeURIComponent(real_json['trigger_id'])
+                
+                
+            
+            //console.log(data);
+
+
+            const options = {
+                hostname: 'slack.com',
+                path: '/api/views.open', 
+                method: 'POST',
+                headers: {
+                    // 'Content-type': 'application/json; charset=UTF-8',
+                    'Content-type': 'application/x-www-form-urlencoded',
+
+
+                    'Content-Length': data.length,
+                    // 'Authorization': `Basic ${token}`,
+                    "Authorization": `OAuth ${token}`
+                  }
+            }
+
+
+
+            const req = https.request(options, res => {
+                console.log(`statusCode: ${res.statusCode}`)
+
+                res.on('data', d => {
+                    process.stdout.write(d)
+                })
+            })
+              
+            req.on('error', error => {
+                console.error(error)
+            })
+              
+            req.write(data)
+            req.end()
+
+            
+
+            // http.request()
+        } else if(real_json['type'] == 'view_submission') {
+            const channelId = real_json['user']['id'];
+            let hoursInput = real_json['view']['state']['values']['hours']['hours']['value'];
+            if(isNaN(hoursInput)) {return}
+            const hours = parseFloat(hoursInput);
+            const task = real_json['view']['state']['values']['task']['task']['value']
+            const blks = getSubmittedDm(hours,task)
+           try{
+               let boi = await post.chat.postMessage({channel: channelId, text: blks})
+               //console.log(boi);
+           } catch (e) {console.log(e)}
+           handleHoursRequest(channelId, hours, 0, task)
+
+        } else if(real_json['type']=='block_actions') {
+
+            let ts = real_json['message']['ts'];
+            const oldBlocks = real_json['message']['blocks'];
+            // hBody = [oldBlocks[0],oldBlocks[1]]
+
+            let addId = real_json['actions'][0]['value'];
+
+            if(addId==='decline') {
+                try{post.chat.update({
+                    channel: real_json['container']['channel_id'],
+                    ts: ts,
+                    blocks: [oldBlocks[0],oldBlocks[1], declinedFooter,
+                    {
+                        "type": "divider"
+                    }]
+                })} catch (err) {console.log(err)}
+                // if(dmRejection){post.chat.postMessage({channel: DATA['e'][addId]['userId'], text: getDeclinedDm(DATA['myLittlePogchamp'],DATA['e'][addId]['time'],DATA['e'][addId]['activity'])})}
+            } else {
+                try{
+                post.chat.update({
+                    channel: real_json['container']['channel_id'],
+                    ts: ts,
+                    blocks: [oldBlocks[0],oldBlocks[1], addedFooter,
+                    {
+                        "type": "divider"
+                    }]
+                })
+            } catch (err) {console.log(err)}
+
+                // INTERFACE WITH GOOGLE API
+                // parseInt(real_json['actions'][0]['value'])
+                post.chat.postMessage({channel: DATA['e'][addId]['userId'], text: getAcceptedDm(DATA['myLittlePogchamp'],DATA['e'][addId]['time'],DATA['e'][addId]['activity'])})
+                addhours(DATA['e'][addId]['name'],DATA['e'][addId]['time'])
+                delete DATA['e'][addId]
+            }
+
+            
+
+
+        }
+
+
+
+    } else if(requestDict['command'] === '/log') {
+        let mins=0, hours=0, actStart=0;
+        let args = requestDict['text'].split(" ")
+        if(args.length===0 || args[0]==='') {
+            post.views.open({
+                view: log_modal,
+                trigger_id: requestDict['trigger_id']})
+        } else if(args.length===1) {
+
+        } else {
+            if(!isNaN(args[0].slice(0,args[0].length-1))) {
+                console.log("sdfdgersdgSDJFHEJGHWEUIAGHERUISGJ STUFFFFFFF")
+                let val = parseFloat(args[0])
+                if(args[0].slice(-1)==='h') {
+                    hours = val;
+                    actStart+=1;
+                } else if(args[0].slice(-1)==='m') {
+                    mins = val;
+                    actStart+=1;
+                }
+            }
+            if(!isNaN(args[1].slice(0,args[1].length-1))) {
+                let val = parseFloat(args[1])
+                if(args[1].slice(-1)==='h') {
+                    hours = val;
+                    actStart+=1;
+                } else if(args[1].slice(-1)==='m') {
+                    mins = val;
+                    actStart+=1;
+                }
+            }
+            let activity = args.slice(actStart,args.length).join(' ');
+            let msg_txt = getSubmittedDmHrsAndMins(hours,mins,activity);
+            let channelId = requestDict['channel_id'];
+            let userId = requestDict['user_id'];
+            try{
+                let boi = await post.chat.postEphemeral({channel: channelId, text: msg_txt, user: userId})
+                //console.log(boi);
+            } catch (e) {console.log(e)}
+            try{
+                let boi = await post.chat.postMessage({channel: userId, text: msg_txt})
+                //console.log(boi);
+            } catch (e) {console.log(e)}
+            
+            handleHoursRequest(userId, hours, mins, activity)
+            
+        }
+    }
+
+    
+    return 
+}
+});
+
+slashServer.listen(slash_port);
+
+
+
+
+
+
+
+
+
+
+
