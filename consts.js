586873b212733060ebadbe0a03a15765514389fc
= <=>
2021-02-27 20:31:59 -0800

    Switch to real sheet id (for merge)

--- consts.js
+++ consts.js
@@ -8,8 +8,8 @@ export const global_hours_label_column = 4
 export const global_hours_value_column = 5
 export const name_column = 0;
 export const max_row = 41;
-// export const hours_sheet_id = '10AcplDpfbXlECQYaFuTFcIeN2U8raP9XysuN3e31js0' // REAL SHEET
-export const hours_sheet_id = '1HrqjjiX9Hghol3ugSFyL8Hslag59cUP88hAgJHW-TUU' // TEST SHEET
+export const hours_sheet_id = '10AcplDpfbXlECQYaFuTFcIeN2U8raP9XysuN3e31js0' // REAL SHEET
+// export const hours_sheet_id = '1HrqjjiX9Hghol3ugSFyL8Hslag59cUP88hAgJHW-TUU' // TEST SHEET
 export const dmRejection = true;
 export const initing = true;
 
