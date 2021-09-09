b9f979f939a1f80fe9f8288644334d3ac67c4e31
MeechaPooch <powchm@catlin.edu>
2021-09-09 15:17:31 -0700

    debug messages formula

--- index.js
+++ index.js
@@ -90,6 +90,7 @@ async function addhours(name, hours) {
 
         if (name.includes(name_cell.value) && name_cell.value != "" && name_cell.value != " ") {
             const hours_cell = sheet.getCell(y, hours_column)
+            console.log("BOI!!!!   " + hours_cell.formula)
             if(hours_cell.formula == null) {
                 if(hours_cell.value) {
                     hours_cell.formula = `=${hours_cell.value}`
@@ -97,6 +98,7 @@ async function addhours(name, hours) {
                     hours_cell.formula = `=`
                 }
             }
+            console.log("BOI2!!!!   " + hours_cell.formula)
             hours_cell.formula = `${hours_cell.formula}+${parseFloat(hours).toFixed(1)}`
             hours_cell.save()
             return
