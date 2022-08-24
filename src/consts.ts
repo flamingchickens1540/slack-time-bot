b04701fd4f17da11cdbe62571f45d7a06f6ac2d3
Zach R <zarutman+git@gmail.com>
2022-08-23 19:39:15 -0700

    remove unnecessary constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,6 @@
 import type { SlackActionMiddlewareArgs, BlockAction, ButtonAction } from "@slack/bolt";
 
-export const bot_port = 9191;
-export const slash_port = 9190;
 export const json_data_path = 'data.json'
-export const json_hours_record_path = "hours.json" 
 export const slack_approver_id = "U03V72L4K4H"
 export const log_sheet_name = "Log"
 export const hours_sheet_id = '1KxS5gqUhygWnLlga9wM2k6jfJr5tCKCxycN3axkWnCA'
