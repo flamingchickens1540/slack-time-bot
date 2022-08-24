1f21c52d790e6b8f1609f58f617ae6c0f337dfc3
Zach R <zarutman+git@gmail.com>
2022-08-23 19:45:39 -0700

    add reminder comment about test IDs

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,10 @@
 import type { SlackActionMiddlewareArgs, BlockAction, ButtonAction } from "@slack/bolt";
 
+// TODO: Update to real sheet and approver ID
 export const json_data_path = 'data.json'
-export const slack_approver_id = "U03V72L4K4H"
+export const slack_approver_id = "U03V72L4K4H" // TEST USER
 export const log_sheet_name = "Log"
-export const hours_sheet_id = '1KxS5gqUhygWnLlga9wM2k6jfJr5tCKCxycN3axkWnCA'
+export const hours_sheet_id = '1KxS5gqUhygWnLlga9wM2k6jfJr5tCKCxycN3axkWnCA' // TEST SHEET
 
 
 export type ButtonActionMiddlewareArgs = SlackActionMiddlewareArgs<BlockAction<ButtonAction>>;
