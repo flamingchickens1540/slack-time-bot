040d2b7691410aa5501fa4c82da9c09528cd68f3
Zach R <zarutman+git@gmail.com>
2022-08-27 14:55:22 -0700

    add example secrets files
    allows the github action to build the project

---
+++ secrets/consts.example.js
@@ -0,0 +1,4 @@
+// The ID of the google sheet to log hours data to
+export const hours_sheet_id = 'googlesheetid'
+// The ID of a slack user to show admin settings to
+export const slack_admin_id = "USLACKUSER" 
---
+++ secrets/slack_secrets.example.js
@@ -0,0 +1,5 @@
+// These constants can be found in the slack app settings.
+
+export const signing_secret = 'signing secret';
+export const token = 'xoxb-token';
+export const app_token = 'xapp-token'
