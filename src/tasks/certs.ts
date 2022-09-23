f0e0955d59f97ac4948816f55092dba241446837
MeechaPooch <powchm@catlin.edu>
2022-09-22 18:17:53 -0700

    swonkier cert messages

--- src/tasks/certs.ts
+++ src/tasks/certs.ts
@@ -4,6 +4,15 @@ import { Certification, Member } from "../types";
 import { certs, saveData } from "../utils/data";
 import { getMembers } from "../utils/drive";
 
+const congratsMessages = [
+    "Hey! Congrats @ for you new {} Cert!",
+    "Awww, @ just got a {} Cert... They hatch so fast [;",
+    "@. {}. Well done.",
+    "Bawk bawk, bawk bawk @ bawk {} bawk, bawk SKREEEEE",
+    "Friends! @ has earned a {}. May we all feast and be merry. :shallow_pan_of_food: ",
+    "Congrats to @ on getting a {} certification!",
+    "@ just earned a {}. Did you know: Software is the bread and butter of robotics.",
+    ]
 
 export async function celebrateMembers(client: WebClient) {
     const slackMembers = (await client.users.list()).members
@@ -20,7 +29,11 @@ export async function celebrateMembers(client: WebClient) {
             const user = slackMembers?.find((slack_member) => slack_member.real_name == member.name)
             const userText = (user == null) ? member.name : `<@${user.id}>`;
             newCerts.forEach(async (cert) => {
-                await client.chat.postMessage({ channel: slack_celebration_channel, text: `${userText} just got a ${cert.name} certification` })
+                let message = congratsMessages[Math.floor(Math.random()*congratsMessages.length)]; // get random message
+                message = message.replace('@',userText) // set user mention
+                message = message.replace('{}',`*${cert.name}*`) // set cert name in *bold*
+
+                await client.chat.postMessage({ channel: slack_celebration_channel, text: message })
             })
         }
         certs[member.name] = member.certs
